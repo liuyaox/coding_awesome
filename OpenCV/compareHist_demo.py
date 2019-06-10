eef654397fca5da3be5fdd85f6f0f2ae7465a1a6
liuyaox <siberiawolf61@hotmail.com>
2019-06-10 11:27:39 +0800

    update methods in compareHist

--- OpenCV/compareHist_demo.py
+++ OpenCV/compareHist_demo.py
@@ -18,11 +18,11 @@ h1_n = minmax_scale(h1)
 h2_n = minmax_scale(h2)
 
 # 遍历各种Metricss
-methods = {cv2.HISTCMP_CORREL: (0, '相关系数'), cv2.HISTCMP_CHISQR: (1, '卡方'), 
-           cv2.HISTCMP_INTERSECT: (2, '十字'), cv2.HISTCMP_BHATTACHARYYA: (3, '巴氏系数'), 
-           cv2.HISTCMP_HELLINGER: (3, '同巴氏系数'), cv2.HISTCMP_CHISQR_ALT: (4, '调整的卡方'), 
-           cv2.HISTCMP_KL_DIV: (5, 'KL散度or相对熵')}
-for method, (method_id, method_name) in methods.items():
+methods = [(cv2.HISTCMP_CORREL, 0, '相关系数'), (cv2.HISTCMP_CHISQR, 1, '卡方'), 
+           (cv2.HISTCMP_INTERSECT, 2, '十字'), (cv2.HISTCMP_BHATTACHARYYA, 3, '巴氏系数'), 
+           (cv2.HISTCMP_HELLINGER, 3, '同巴氏系数'), (cv2.HISTCMP_CHISQR_ALT, 4, '调整的卡方'), 
+           (cv2.HISTCMP_KL_DIV, 5, 'KL散度or相对熵')]
+for method, method_id, method_name in methods:
     print('Method-' + str(method) + ': ' + str(round(cv2.compareHist(h1_n, h2_n, method), 4)), method_name)
 
 
